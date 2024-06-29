aa7f0218f42abc27e13b6d1245ff068dae9d3321
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-29 01:58:49 -0700

    Grant 'All' and 'Specific' privileges to a User, try 1: add wildcard to dbName.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,16 +9,10 @@ CREATE USER
 IF NOT EXISTS `hbnb_dev`@`localhost`
 IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Command Setting one particular Database as 'in use'
-USE `hbnb_dev_db`;
-
 -- Command granting All Privileges to user
 GRANT ALL PRIVILEGES
-ON `hbnb_dev_db`
+ON `hbnb_dev_db`.*
 TO `hbnb_dev`@`localhost`;
-
--- Command Setting another databse in Use
-USE `performance_schema`;
 
 -- Command granting particular privilege to User
 GRANT SELECT
