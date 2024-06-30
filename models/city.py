63eabd3a76b2e9b71ff6284e7e2be1ebe37064eb
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-30 15:42:42 -0700

    Update the place model, try 2.

--- models/city.py
+++ models/city.py
@@ -11,5 +11,8 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
 
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
+    state_id = Column(String(60), ForeignKey(
+        'states.id', ondelete='CASCADE'), nullable=False)
+    places = relationship(
+        "models.place.Place", backref="cities",
+        cascade='all, delete-orphan')
