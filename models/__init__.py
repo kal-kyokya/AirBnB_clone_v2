842be81ef93c27ed5a004ba1b0d961f7f17681a3
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-30 08:30:03 -0700

    Transition from file to DBStorage and Add attributes for SQLAlchemy, try 3.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
 
 
-storage = FileStorage()
-storage.reload()
+storage_t = os.getenv('HBNB_TYPE_STORAGE')
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+
+
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+
+
+    storage = FileStorage()
+    storage.reload()
