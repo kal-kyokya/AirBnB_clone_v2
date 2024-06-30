3d27fd90552c4e53fab79f966ccb1f483740126e
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-30 14:15:02 -0700

    Transition from file to DBStorage and Add attributes for SQLAlchemy, try 8.

--- models/state.py
+++ models/state.py
@@ -10,4 +10,6 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan",
+                          passive_deletes=True)
