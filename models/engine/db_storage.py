82a2763e1445048c905490e1c37967380926a38f
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-30 08:14:44 -0700

    Transition from file to DBStorage and Add attributes for SQLAlchemy, try 2.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -18,25 +18,29 @@ class DBStorage():
 
     def __init__(self):
         """Initializes class instances' public attributes."""
-        user = os.getenv('HBNB_MYSQL_USER')
-        pwd = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST')
-        db = os.getenv('HBNB_MYSQL_DB')
+        try:
+            user = os.getenv('HBNB_MYSQL_USER')
+            pwd = os.getenv('HBNB_MYSQL_PWD')
+            host = os.getenv('HBNB_MYSQL_HOST')
+            db = os.getenv('HBNB_MYSQL_DB')
 
-        mandatory = [user, pwd, host, db]
-        if not all(mandatory):
-            print("Ensure proper setting of environment variables.")
-            return
+            mandatory = [user, pwd, host, db]
+            if not all(mandatory):
+                print("Ensure proper setting of environment variables.")
+                return
 
-        with DBStorage.__engine as eng:
-            eng = create_engine(
-                "mysql+mysqldb://{}:{}@{}/{}".format(
-                    user, pwd, host, db),
-                pool_pre_ping=True)
-            session_maker = sessionmaker(bind=eng)
-            DBStorage.__session = session_maker()
-            if os.getenv('HBNB_ENV') == 'test':
-                Base.metadata.drop_all(bind=eng, checkfirst=True)
+            with DBStorage.__engine as eng:
+                eng = create_engine(
+                    "mysql+mysqldb://{}:{}@{}/{}".format(
+                        user, pwd, host, db),
+                    pool_pre_ping=True)
+                session_maker = sessionmaker(bind=eng)
+                DBStorage.__session = session_maker()
+                if os.getenv('HBNB_ENV') == 'test':
+                    Base.metadata.drop_all(bind=eng, checkfirst=True)
+        except Exception as err:
+            print("Raised an Exception during init:")
+            print(err)
 
     def all(self, cls=None):
         """Retrieves objects from the database storage.
@@ -55,3 +59,40 @@ class DBStorage():
 
             return {"{}.{}".format(
                 obj.__class__.__name__, obj.id): obj for obj in results}
+
+    def new(self, obj):
+        """Adds an object to the current Database session.
+
+        Arg:
+            obj: Name of the object to be added to the Database.
+        """
+        if obj and self.__session:
+            self.__session.add(obj)
+
+    def save(self):
+        """Commits all changes to the current Database session."""
+        if self.__session:
+            self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete an object from the current Database session.
+
+        Arg:
+            obj: Name of the object to be deleted.
+        """
+        if obj and self.__session:
+            session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database and also
+        create the current database session using 'sessionmaker'
+        and 'scoped_session' to make the Session thread-safe.
+        """
+        try:
+            with self.engine as eng:
+                Base.metadata.create_all(eng)
+                session_maker = sessionmaker(eng, expire_on_commit=False)
+                session = session_maker()
+        except Exception as err:
+            print("Exception raised during reload:")
+            print(err)
