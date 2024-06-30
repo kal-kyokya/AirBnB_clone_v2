8e1cad2aee61723fb95e65492ec2b3509f1d3e11
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-30 08:35:19 -0700

    Transition from file to DBStorage and Add attributes for SQLAlchemy, try 4.

--- models/__init__.py
+++ models/__init__.py
@@ -7,12 +7,9 @@ storage_t = os.getenv('HBNB_TYPE_STORAGE')
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
 
-
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
 
-
     storage = FileStorage()
-    storage.reload()
+storage.reload()
