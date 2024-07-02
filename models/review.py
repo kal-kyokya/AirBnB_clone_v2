faea42630bfd26bdfc8578a24960a0ab34db86bb
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-02 06:17:52 -0700

    Update 'Review' model.

--- models/review.py
+++ models/review.py
@@ -2,6 +2,8 @@
 """This is the review class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
+from os import getenv
+import models
 
 
 class Review(BaseModel, Base):
@@ -11,13 +13,18 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
     """
-    __tablename__ = "reviews"
-    place_id = Column(String(60),
-                      ForeignKey('places.id',
-                                 ondelete='CASCADE'),
-                      nullable=False)
-    user_id = Column(String(60),
-                     ForeignKey('users.id',
-                                ondelete='CASCADE'),
-                     nullable=False)
-    text = Column(String(1024), nullable=False)
+    if models.storage_t == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60),
+                          ForeignKey('places.id',
+                                     ondelete='CASCADE'),
+                          nullable=False)
+        user_id = Column(String(60),
+                         ForeignKey('users.id',
+                                    ondelete='CASCADE'),
+                         nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
