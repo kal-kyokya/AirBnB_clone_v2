ffe1ba7567eef85c0b1654cf6abb6a6a9b1a7f45
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-18 16:14:40 -0700

    Create Python function, executing fabric command that archive directory.

---
+++ 1-pack_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+'1-pack_web_static' is '.tgz archive' file creation script
+written in python to run using the 'fab' command.
+"""
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Creates a tap archive file aka tar.
+
+    Args:
+        None.
+    Return:
+    	The path to the created .tgz archive or None if error.
+    """
+
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+        
+        if local("tar -cvzf {} web_static".format(file)).failed is True:
+            return None
+        return file
