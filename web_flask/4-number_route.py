6de9a4fe71ce77f1d653d2dc92ff6b848a928378
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-31 05:49:52 -0700

    Handle Web Application's variable type filtration.

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+'4-number_route' is a Flask web application starting module
+"""
+# Import the Flask class from the flask module
+from flask import Flask
+
+
+# Specify the current file as the web app controller
+app = Flask(__name__)
+
+
+# Define route handling mechanisms
+@app.route("/", strict_slashes=False)
+def home():
+    """Function handling '/' route requests.
+
+    Return:
+        A string text.
+    """
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Function called when the '/hbnb' route is taken.
+
+    Return:
+        A string text.
+    """
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Function used when the '/c/' route is used with a URL text input.
+
+    Arg:
+        text: URL text Input to be used.
+
+    Return:
+        A string text.
+    """
+    return ("C " + text.replace('_', ' '))
+
+
+@app.route("/python/", strict_slashes=False, defaults={'text': "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Function used when the '/python/' route is used with a URL  text input.
+
+    Arg:
+        text: URL text Input to be used.
+
+    Return:
+        A string text.
+    """
+    return ("Python " + text.replace('_', ' '))
+
+
+@app.route("/number/<int:num>", strict_slashes=False)
+def number(num):
+    """Function for the '/number/' route with a URL integer input.
+
+    Arg:
+        num: URL integer Input to be used.
+
+    Return:
+        A string text.
+    """
+    print("num value is:")
+    print(num, end="\n\n")
+    return (f"{num} is a number")
+
+
+if __name__ == "__main__":
+    app.run()
