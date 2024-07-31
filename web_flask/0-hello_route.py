23523fe8a37704715f80023e40aee1a580df0ac5
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-31 04:16:14 -0700

    Write a Python3 script starting a Flask Web Application.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+'0-hello_route' is a Flask web application starting script
+"""
+from flask import Flask
+
+
+# Turn the current file into a Framework controller
+app = Flask(__name__)
+
+# Define actions ensuing access to the 'home page'
+@app.route("/", strict_slashes=False)
+def home():
+    """Function to be called when the '/' route is taken."""
+
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run()
